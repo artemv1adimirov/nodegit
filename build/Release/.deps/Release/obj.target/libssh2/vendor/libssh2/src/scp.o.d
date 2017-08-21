8b09771a9c90475e60637a7b42b77c5c36b0879d
Artem Vladimirov <artem.vladimirov@liflandgaming.com>
2017-08-21 14:46:27 +0300

    try 3

---
+++ build/Release/.deps/Release/obj.target/libssh2/vendor/libssh2/src/scp.o.d
@@ -0,0 +1,90 @@
+cmd_Release/obj.target/libssh2/vendor/libssh2/src/scp.o := cc '-DNODE_GYP_MODULE_NAME=libssh2' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_DARWIN_USE_64_BIT_INODE=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DNETSNMP_ENABLE_IPV6' -I/Users/artemvladimirov/.node-gyp/8.2.1/include/node -I/Users/artemvladimirov/.node-gyp/8.2.1/src -I/Users/artemvladimirov/.node-gyp/8.2.1/deps/uv/include -I/Users/artemvladimirov/.node-gyp/8.2.1/deps/v8/include -I../vendor -I../vendor/libssh2/include -I../vendor/openssl/openssl/include  -Os -gdwarf-2 -mmacosx-version-min=10.7 -arch x86_64 -Wall -Wendif-labels -W -Wno-unused-parameter -fno-strict-aliasing -MMD -MF ./Release/.deps/Release/obj.target/libssh2/vendor/libssh2/src/scp.o.d.raw   -c -o Release/obj.target/libssh2/vendor/libssh2/src/scp.o ../vendor/libssh2/src/scp.c
+Release/obj.target/libssh2/vendor/libssh2/src/scp.o: \
+  ../vendor/libssh2/src/scp.c ../vendor/libssh2/src/libssh2_priv.h \
+  ../vendor/libssh2/src/libssh2_config.h \
+  ../vendor/libssh2/include/libssh2.h \
+  ../vendor/libssh2/include/libssh2_publickey.h \
+  ../vendor/libssh2/include/libssh2_sftp.h ../vendor/libssh2/src/misc.h \
+  ../vendor/libssh2/src/crypto.h ../vendor/libssh2/src/openssl.h \
+  /Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/opensslconf.h \
+  /Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/./archs/darwin64-x86_64-cc/opensslconf.h \
+  /Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/sha.h \
+  /Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/e_os2.h \
+  /Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/rsa.h \
+  /Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/asn1.h \
+  /Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/bio.h \
+  /Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/crypto.h \
+  /Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/stack.h \
+  /Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/safestack.h \
+  /Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/opensslv.h \
+  /Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/ossl_typ.h \
+  /Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/symhacks.h \
+  /Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/bn.h \
+  /Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/engine.h \
+  /Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/dsa.h \
+  /Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/dh.h \
+  /Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/ecdh.h \
+  /Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/ec.h \
+  /Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/ecdsa.h \
+  /Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/rand.h \
+  /Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/ui.h \
+  /Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/err.h \
+  /Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/lhash.h \
+  /Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/x509.h \
+  /Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/buffer.h \
+  /Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/evp.h \
+  /Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/objects.h \
+  /Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/obj_mac.h \
+  /Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/x509_vfy.h \
+  /Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/pkcs7.h \
+  /Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/md5.h \
+  /Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/hmac.h \
+  /Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/pem.h \
+  /Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/pem2.h \
+  ../vendor/libssh2/src/channel.h ../vendor/libssh2/src/session.h
+../vendor/libssh2/src/scp.c:
+../vendor/libssh2/src/libssh2_priv.h:
+../vendor/libssh2/src/libssh2_config.h:
+../vendor/libssh2/include/libssh2.h:
+../vendor/libssh2/include/libssh2_publickey.h:
+../vendor/libssh2/include/libssh2_sftp.h:
+../vendor/libssh2/src/misc.h:
+../vendor/libssh2/src/crypto.h:
+../vendor/libssh2/src/openssl.h:
+/Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/opensslconf.h:
+/Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/./archs/darwin64-x86_64-cc/opensslconf.h:
+/Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/sha.h:
+/Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/e_os2.h:
+/Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/rsa.h:
+/Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/asn1.h:
+/Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/bio.h:
+/Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/crypto.h:
+/Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/stack.h:
+/Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/safestack.h:
+/Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/opensslv.h:
+/Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/ossl_typ.h:
+/Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/symhacks.h:
+/Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/bn.h:
+/Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/engine.h:
+/Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/dsa.h:
+/Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/dh.h:
+/Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/ecdh.h:
+/Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/ec.h:
+/Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/ecdsa.h:
+/Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/rand.h:
+/Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/ui.h:
+/Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/err.h:
+/Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/lhash.h:
+/Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/x509.h:
+/Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/buffer.h:
+/Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/evp.h:
+/Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/objects.h:
+/Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/obj_mac.h:
+/Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/x509_vfy.h:
+/Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/pkcs7.h:
+/Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/md5.h:
+/Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/hmac.h:
+/Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/pem.h:
+/Users/artemvladimirov/.node-gyp/8.2.1/include/node/openssl/pem2.h:
+../vendor/libssh2/src/channel.h:
+../vendor/libssh2/src/session.h:
